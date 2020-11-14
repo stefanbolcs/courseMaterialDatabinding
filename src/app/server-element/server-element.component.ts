f023e3a573fdf1c2be420de9bf06bae6a0f2f06a
Stefan B <stefanbolcs@gmail.com>
2020-11-14 16:08:32 +0100

    simple ViemEncapsulation for css styles

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  //this element wont have ViewEncapsulation so to thei tags  wont be any ngcontent added when looking at HTML in browser
+  //if its none it will go rule globally
+   encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element:{
